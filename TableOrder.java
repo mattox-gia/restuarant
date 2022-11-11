54bb98dcce3ec6f7c1fadcf71048f2aead079aba
Gia Mattox <gmattox26@gmail.com>
2022-11-11 08:50:58 -0600

    f

--- TableOrder.java
+++ TableOrder.java
@@ -1,4 +1,20 @@
-public class TableOrder extends Order{
+public class TableOrder extends Order {
     private int tableNumber;
+
+    public TableOrder(int tableNumber){
+        super();
+        this.tableNumber  = tableNumber;
+    }
+
+    public int getTableNumber() {
+        return tableNumber;
+    }
+
+    public void setTableNumber(int tableNumber) {
+        this.tableNumber = tableNumber;
+    }
+
+    
+
 
 }
